706c6a4d1ee5745390c77ce5405b5751a0440537
Fausto Bernard Melo Soares <fausto.ifs@gmail.com>
2022-03-01 11:26:26 -0300

    Gerando setters e getters atributos numero e agencia

--- src/br/com/banco/ContaCorrente.java
+++ src/br/com/banco/ContaCorrente.java
@@ -4,6 +4,20 @@ public class ContaCorrente {
 	
 	protected String numero;
 	protected int agencia;
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	public int getAgencia() {
+		return agencia;
+	}
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
+	
+	
 	
 
 }
