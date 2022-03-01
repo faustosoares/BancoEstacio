af66521ca83552ee9bd54d73c5aee59be41adca1
Fausto Bernard Melo Soares <fausto.ifs@gmail.com>
2022-03-01 11:27:26 -0300

    Gerando construtores

--- src/br/com/banco/ContaCorrente.java
+++ src/br/com/banco/ContaCorrente.java
@@ -3,7 +3,20 @@ package br.com.banco;
 public class ContaCorrente {
 	
 	protected String numero;
-	protected int agencia;
+	protected int agencia;	
+	
+	
+	public ContaCorrente() {
+		super();
+	}
+	
+	public ContaCorrente(String numero, int agencia) {
+		super();
+		this.numero = numero;
+		this.agencia = agencia;
+	}
+
+
 	public String getNumero() {
 		return numero;
 	}
